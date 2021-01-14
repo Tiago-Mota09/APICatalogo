28dd112ac434a6ceb8c3fbcd77c32cc679c0bd71
Tiago Mota <71279207+Tiago-Mota09@users.noreply.github.com>
2021-01-14 17:43:47 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
